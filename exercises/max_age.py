a5fd603dde6c90db7bd9e8b186efdbf7657f8f36
Ester Stankovska <stankovskaester@gmail.com>
2024-05-20 21:03:50 +0200

    exercises.

---
+++ exercises/max_age.py
@@ -0,0 +1,15 @@
+jack_age = int(input())
+alex_age = int(input())
+lana_age = int(input())
+
+min1 = 0
+min1 = jack_age #2
+
+if alex_age < jack_age: #82
+    min1 = alex_age
+if lana_age < jack_age:
+    min1 = lana_age
+
+
+print(min1)
+
---
+++ exercises/sum_f.py
@@ -0,0 +1,5 @@
+x = int(input())
+y = int(input())
+
+# Use the sum function to calculate the sum of x and y
+print(sum([x, y])) # some takes only iterable objects
